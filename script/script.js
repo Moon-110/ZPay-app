f2a5bae7aacbb159c549c4f6b14676da3d6da1ab
Moon110 <moynul1.islam0@gmail.com>
2025-09-05 00:09:14 +0600

    login button functionality

---
+++ script/script.js
@@ -0,0 +1,6 @@
+// login button functionality
+document.getElementById('btn-login').addEventListener('click', function (e){
+    e.preventDefault();
+    console.log('login btn clicked');
+    console.log(e);
+})
